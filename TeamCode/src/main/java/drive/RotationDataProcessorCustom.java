b2f107d663119c83aabb546f15637bfbd43e2625
GigaBiteRobotics <gigabiterobotics@gmail.com>
2025-02-13 15:57:03 -0800

    Trying to get RotationDataProcessorCustom to get the correct data throughput & set positions
    (cherry picked from commit 0f69c4b43cd7a4901be063b947906aaaa5a7dfaa)

--- TeamCode/src/main/java/drive/RotationDataProcessorCustom.java
+++ TeamCode/src/main/java/drive/RotationDataProcessorCustom.java
@@ -1,11 +1,9 @@
 package drive;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RotationDataProcessorCustom {
-	public static ArrayList<Double> dataPoints = new ArrayList<>();
+	private ArrayList<Double> dataPoints = new ArrayList<>();
 	private static final double outlierThreshold = 1.5;
 
 	public void addDataPoint(double dataPoint) {
@@ -14,10 +12,17 @@ public class RotationDataProcessorCustom {
 	public void clearDataPoints() {
 		dataPoints.clear();
 	}
+	public double getAverage() {
+		double sum = 0;
+		for (double point : dataPoints) {
+			sum += point;
+		}
+		return sum / dataPoints.size();
+	}
 	public void removeOutliers() {
 		if (dataPoints.size() < 3) return;
 		double mean = getAverage();
-		double standardDeviation = calculateStandardDeviation(dataPoints);
+		double standardDeviation = calculateStandardDeviation(dataPoints, mean);
 		ArrayList<Double> filteredData = new ArrayList<>();
 		for (double value : dataPoints) {
 			if (Math.abs(value - mean) <= outlierThreshold * standardDeviation) {
@@ -26,50 +31,11 @@ public class RotationDataProcessorCustom {
 		}
 		dataPoints = filteredData;
 	}
-
-
-	public static double getAverage() {
+	private double calculateStandardDeviation(ArrayList<Double> data, double mean) {
 		double sum = 0;
-		for (double value : dataPoints) {
-			sum += value;
+		for (double value : data) {
+			sum += Math.pow(value - mean, 2);
 		}
-		return sum / dataPoints.size();
-	}
-	public double getMode() {
-		if (dataPoints == null || dataPoints.isEmpty()) {
-			return Double.NaN;
-		}
-
-		HashMap<Double, Integer> frequencyMap = new HashMap<>();
-		for (double num : dataPoints) {
-			frequencyMap.merge(num, 1, Integer::sum);
-		}
-
-		double mode = Double.NaN;
-		int maxCount = 0;
-		for (Map.Entry<Double, Integer> entry : frequencyMap.entrySet()) {
-			if (entry.getValue() > maxCount) {
-				maxCount = entry.getValue();
-				mode = entry.getKey();
-			}
-		}
-		return mode;
-	}
-	public double calculateStandardDeviation(ArrayList<Double> data) {
-		double mean = getAverage();
-		double sumSquaredDifferences = 0.0;
-
-		for (double num : data) {
-			sumSquaredDifferences += Math.pow(num - mean, 2);
-		}
-
-		double variance = sumSquaredDifferences / data.size()
-				;
-		return Math.sqrt(variance);
-	}
-	public void removeOldDataPoints(int maxDataPoints) {
-		while (dataPoints.size() > maxDataPoints) {
-			dataPoints.remove(0);
-		}
+		return Math.sqrt(sum / data.size());
 	}
 }
